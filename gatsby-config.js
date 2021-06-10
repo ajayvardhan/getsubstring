a0fec40eab8a0241ad756d89ebbe497d1286f72b
Ajay <ajyvardan@gmail.com>
2021-06-10 13:41:20 +0530

    add material UI and template

--- gatsby-config.js
+++ gatsby-config.js
@@ -2,5 +2,5 @@ module.exports = {
   siteMetadata: {
     title: "Get Substring",
   },
-  plugins: ["gatsby-plugin-gatsby-cloud"],
+  plugins: ["gatsby-plugin-gatsby-cloud", `gatsby-plugin-material-ui`],
 };
